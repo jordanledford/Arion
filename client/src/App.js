a505ca7fbaeaec0a672da519bb122323e71a59e6
J <jordanmledford@gmail.com>
2017-01-30 13:29:24 -0500

    initial import of react / material-ui because i'm trendy

---
+++ client/src/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <h1>Arion</h1>
+          <h3>An Instagram Tool</h3>
+        </div>
+        <p className="App-intro">
+          <code>view metadata about your instagram photos!!!</code>
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
